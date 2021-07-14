66b4b69a5590843c33e9aa301b83a8bb2d60f6f4
athena <athena@ciandt.com>
2021-07-14 17:26:55 -0300

    finish project

--- Hobbies/ViewController.swift
+++ Hobbies/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hobbiesButton: UIButton!
+    
+    @IBOutlet weak var routineView: UIView!
+    
+    @IBOutlet weak var hobbiesView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func callRoutine(_ sender: Any) {
+        hobbiesView.alpha = 0
+        hobbiesView.isHidden = true
+        routineView.alpha = 1
+        routineView.isHidden = false
+    }
+    
+    @IBAction func callHobies(_ sender: Any) {
+        hobbiesView.alpha = 1
+        hobbiesView.isHidden = false
+        routineView.alpha = 0
+        routineView.isHidden = true
+    }
+    
+    
 }
 
